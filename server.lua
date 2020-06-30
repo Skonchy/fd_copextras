f0c0073c8e7a763887a9b8b23944d368730ee4bc
Skonchy <mrskonchy@gmail.com>
2020-06-29 21:19:02 -0500

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,22 +2,24 @@ RegisterServerEvent("fd_copextras:checkOnDuty")
 AddEventHandler("fd_copextras:checkOnDuty", function()
     local src = source
     local char = exports["drp_id"]:GetCharacterData(src)
-    exports["externalsql"]:AsyncQueryCallback({
-        query = [[SELECT * FROM characters WHERE id = :charid]],
-        data = {
-            charid = char.charid
-        }
-    }, function(results)
-        if results.data == nil then
-            TriggerClientEvent("DRP_Core:Error",src,"Cop Extras",tostring("Couldnt find character id in db"),4500,false,"leftCenter")
-        else
-            if results.data[1].job == "POLICE" then
-                TriggerClientEvent("fd_copextras:toggleOnDuty", src, true)
+    if char ~= false then
+        exports["externalsql"]:AsyncQueryCallback({
+            query = [[SELECT * FROM characters WHERE id = :charid]],
+            data = {
+                charid = char.charid
+            }
+        }, function(results)
+            if results.data == nil then
+                TriggerClientEvent("DRP_Core:Error",src,"Cop Extras",tostring("Couldnt find character id in db"),4500,false,"leftCenter")
             else
-                TriggerClientEvent("fd_copextras:toggleOnDuty", src, false)
+                if results.data[1].job == "POLICE" then
+                    TriggerClientEvent("fd_copextras:toggleOnDuty", src, true)
+                else
+                    TriggerClientEvent("fd_copextras:toggleOnDuty", src, false)
+                end
             end
-        end
-    end)
+        end)
+    end
 end)
 
 function dump(o)
